03a019620fd3b66f7e0024ea50c65cc328cdc5b6
luheresbar <luheresbar@gmail.com>
2025-02-12 19:52:45 -0500

    first commit

---
+++ 02. spring-boot-profiles/src/main/java/com/environment/persistence/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.environment.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    private Long id;
+    private String name;
+}
---
+++ 02. spring-boot-profiles/src/main/java/com/environment/persistence/repository/IEmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.environment.persistence.repository;
+
+import com.environment.persistence.entity.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IEmployeeRepository extends CrudRepository<Employee, Long> {
+}
